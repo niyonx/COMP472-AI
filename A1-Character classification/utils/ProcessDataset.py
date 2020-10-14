1ec3d09c9325ee4ca7a27dd05ec5097bca375edf
niyonx <kenkenysy@gmail.com>
2020-10-14 00:59:29 -0400

    Perceptron model check

--- A1-Character classification/utils/ProcessDataset.py
+++ A1-Character classification/utils/ProcessDataset.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import config
+from sklearn.metrics import precision_recall_fscore_support
 
 
 class character:
@@ -17,8 +18,11 @@ class character:
 
 
 def get_DataFromCSV(filename):
-    headers = np.append(np.arange(0, 1024), 'CHARACTER')
     data = pd.read_csv(os.path.join(config.DATASET_PATH, filename))
+    if(data.shape[1] == 1025):
+        headers = np.append(np.arange(0, 1024), 'CHARACTER')
+    else:
+        headers = np.arange(0,1024)
     data.columns = headers
     return data
 
@@ -26,3 +30,13 @@ def get_DataFromCSV(filename):
 def countDistribution(filename):
     dataDist = get_DataFromCSV(filename)['CHARACTER'].value_counts()
     return dataDist.to_dict()
+
+def calculate_PRF(y_true, y_pred):
+    PRF = precision_recall_fscore_support(y_true, y_pred)
+    precision = PRF[0].reshape(-1,1)
+    recall = PRF[1].reshape(-1,1)
+    F1 = PRF[2].reshape(-1,1)
+
+    PRF_table = pd.DataFrame(np.concatenate((precision, recall, F1), axis=1))
+    PRF_table.columns=['precision','recall','f1']
+    return PRF_table
