5ebc83778794e55ee111e41fa1480c0ed4f7cd56
niyonx <kenkenysy@gmail.com>
2020-11-07 17:06:30 -0500

    helper + ucs fix

--- A2- X-Puzzle/utils/helper.py
+++ A2- X-Puzzle/utils/helper.py
@@ -5,45 +5,48 @@ def move(puzzle, zero_idx, to_idx):
         puzzle = puzzle[:to_idx] + puzzle[zero_idx] + puzzle[to_idx+1:zero_idx] + puzzle[to_idx] + puzzle[zero_idx+1:]
     return puzzle
 
-def find_possible_paths(puzzle, closed=[]):
-    # returns list of all possible paths in form (path, cost, predecessor)
+def find_possible_paths(puzzle, closed=[], columns=4, rows=2):
+    # returns list of all possible paths in form (path, cost, predecessor, token_to_move)
     # TODO: when scaling, softcode values
 
     paths = []
     zero_idx = puzzle.index('0')
-    column = 4
+    last_idx = (columns*rows)-1
+    
 
     # check move up
-    if(zero_idx - 4 >= 0):
-        paths.append((move(puzzle, zero_idx, zero_idx - 4),1, puzzle))
+    if(zero_idx - columns >= 0):
+        paths.append((move(puzzle, zero_idx, zero_idx - columns), 1, puzzle, zero_idx - columns))
     # check move down
-    if(zero_idx + 4 <= 7):
-        paths.append((move(puzzle, zero_idx, zero_idx + 4),1, puzzle))
+    if(zero_idx + columns <= last_idx):
+        paths.append((move(puzzle, zero_idx, zero_idx + columns), 1, puzzle, zero_idx + columns))
     # check move left
     if(zero_idx - 1 >= 0):
-        paths.append((move(puzzle, zero_idx, zero_idx - 1),1, puzzle))
+        if(zero_idx % columns != 0) # moving left might be illegal 12340567 to 12304567
+            paths.append((move(puzzle, zero_idx, zero_idx - 1), 1, puzzle, zero_idx - 1))
     # check move right
-    if(zero_idx + 1 <= 7):
-        paths.append((move(puzzle, zero_idx, zero_idx + 1),1, puzzle))
+    if(zero_idx + 1 <= last_idx):
+        if((zero_idx + 1) % columns != 0) # moving right might be illegal 12304567 to 12340567
+            paths.append((move(puzzle, zero_idx, zero_idx + 1), 1, puzzle, zero_idx + 1))
     # check move diag down-right
-    if(zero_idx + 5 <= 7):
-        paths.append((move(puzzle, zero_idx, zero_idx + 5),3, puzzle))
+    if(zero_idx + (columns + 1) <= last_idx):
+        paths.append((move(puzzle, zero_idx, zero_idx + (columns + 1)), 3, puzzle, zero_idx + (columns + 1)))
     # check move diag down-left
-    if(zero_idx + 3 <= 7):
+    if(zero_idx + (columns - 1) <= last_idx):
         # if zero_idx is in corner, diag move will lead to wrapping move e.g 01234567 to 31204567
-        if(zero_idx % column == 0 or (zero_idx+1) % column == 0): # left corner or right corner
-            paths.append((move(puzzle, zero_idx, zero_idx + 3),2, puzzle))
+        if(zero_idx % columns == 0 or (zero_idx + 1) % columns == 0): # left corner or right corner
+            paths.append((move(puzzle, zero_idx, zero_idx + (columns - 1)), 2, puzzle, zero_idx + (columns - 1)))
         else:
-            paths.append((move(puzzle, zero_idx, zero_idx + 3),3, puzzle))
+            paths.append((move(puzzle, zero_idx, zero_idx + (columns - 1)), 3, puzzle, zero_idx + (columns - 1)))
     # check move diag up-right
-    if(zero_idx - 3 >= 0):
-        if(zero_idx % column == 0 or (zero_idx+1) % column == 0): # left corner or right corner
-            paths.append((move(puzzle, zero_idx, zero_idx - 3),2, puzzle))
+    if(zero_idx - (columns - 1) >= 0):
+        if(zero_idx % columns == 0 or (zero_idx + 1) % columns == 0): # left corner or right corner
+            paths.append((move(puzzle, zero_idx, zero_idx - (columns - 1)), 2, puzzle, zero_idx - (columns - 1)))
         else:
-            paths.append((move(puzzle, zero_idx, zero_idx - 3),3, puzzle))
+            paths.append((move(puzzle, zero_idx, zero_idx - (columns - 1)), 3, puzzle,zero_idx - (columns - 1)))
     # check move diag up-left
-    if(zero_idx - 5 >= 0):
-        paths.append((move(puzzle, zero_idx, zero_idx - 5),3, puzzle))
+    if(zero_idx - (columns + 1) >= 0):
+        paths.append((move(puzzle, zero_idx, zero_idx - (columns + 1)), 3, puzzle, zero_idx - (columns + 1)))
 
     # excludes closed paths
     if closed != []:
