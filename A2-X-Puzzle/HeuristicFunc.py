a0f6e2078fa366473c1e17aa631240f1bb94802a
niyonx <kenkenysy@gmail.com>
2020-11-16 17:16:03 -0500

    Added simple heuristic 1

--- A2-X-Puzzle/HeuristicFunc.py
+++ A2-X-Puzzle/HeuristicFunc.py
@@ -7,3 +7,17 @@ def funcH0(config: puzzle):
     if(config.content[config.last_idx] == 0):
         return 0
     return 1
+
+def funcH1(config: puzzle):
+    max_count = 0
+    count = 0
+    for win in WIN_CONFIG:
+        for i, data in enumerate(win):
+            if(config.content[i] == data):
+                count += 1
+        if(max_count < count):
+            max_count = count
+        count = 0
+    return (config.columns * config.rows) - max_count
+
+# print(funcH1(puzzle([1,2,3,4,7,6,5,0],4,2)))
